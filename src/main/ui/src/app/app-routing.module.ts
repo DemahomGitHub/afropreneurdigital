d8b08d1b539d6337f5996322f272709f5ec2653e
demahom <demahom.toure@gmail.com>
2020-05-18 19:40:56 +0200

    Home page created

--- src/main/ui/src/app/app-routing.module.ts
+++ src/main/ui/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './pages/content/home-page/home-page.component';
+import {ArticlesComponent} from './pages/content/articles/articles.component';
+import {AboutComponent} from './pages/content/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: 'accueil',  component: HomePageComponent },
+  { path: 'articles', component: ArticlesComponent},
+  { path: 'infos', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './pages/header/header.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { MainContentComponent } from './pages/content/main-content/main-content.component';
+import { HomePageComponent } from './pages/content/home-page/home-page.component';
+import { AboutComponent } from './pages/content/about/about.component';
+import { ArticlesComponent } from './pages/content/articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    MainContentComponent,
+    HomePageComponent,
+    AboutComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
